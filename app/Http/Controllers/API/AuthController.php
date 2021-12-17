88b16711cc499c52a9855294656742d7a68f3214
gricosm <guillermo.rico.sm@gmail.com>
2021-12-17 10:52:03 +0100

    Created user login

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -2,12 +2,12 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Models\User;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Hash;
-use Auth;
-use App\Models\User;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Auth;
 
 class AuthController extends Controller
 {
@@ -38,5 +38,20 @@ class AuthController extends Controller
         ]);
     }
 
+    public function login(Request $request)
+    {
+        if(!Auth::attempt($request -> only('email', 'password')))
+        {
+            return $this -> errorResponse(['message' => 'Error en el Login'], Response::HTTP_UNAUTHORIZED);
+        }
 
+        $user = User::where('email', $request['email']) -> firstOrFail();
+
+        $token = $user -> createToken('auth_token') -> plainTextToken;
+
+        return response() -> json([
+            'access_token' => $token,
+            'token_type' => 'Bearer',
+        ]);
+    }
 }
