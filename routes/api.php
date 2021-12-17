a3499e09d3d4a0ef1c39cd9e0d970dd50fc8c9cf
gricosm <guillermo.rico.sm@gmail.com>
2021-12-17 12:13:35 +0100

    Created mode Role and it migration

--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('/register', [App\Http\Controllers\API\AuthController::class, 'register']);
 Route::post('/login', [App\Http\Controllers\API\AuthController::class, 'login']);
-Route::middleware('auth:sanctum') -> get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['middleware' => ['auth:sanctum']], function() {
+    Route::get('/profile', function(Request $request){
+        return auth() -> user();
+    });
+    Route::post('logout', [App\Http\Controllers\API\AuthController::class, 'logout']);
 });
+// Route::middleware('auth:sanctum') -> get('/user', function (Request $request) {
+//     return $request->user();
+// });
